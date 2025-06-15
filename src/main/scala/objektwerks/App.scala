e974ffd5e6033b305e5e9babe1c302f5b9beeccd
objektwerks <objektwerks@runbox.com>
2025-06-15 09:41:29 -0400

    enhanced app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -1,3 +1,15 @@
 package objektwerks
 
-@main def runApp(): Unit = println("app")
+import dev.langchain4j.model.openai.OpenAiChatModel
+
+@main def runApp(): Unit =
+  val openAiApiKey = System.getenv("OPENAI_API_KEY")
+  val modelName = "gpt-4o-mini"
+
+  val model = OpenAiChatModel.builder()
+    .apiKey(openAiApiKey)
+    .modelName(modelName)
+    .build()
+
+  val answer = model.chat("Say 'Hello World'")
+  println(answer)
